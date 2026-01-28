5a58ad34b58b089e381a9441876087d8c6886812
Jonathan D. Backhaus <jonathan.david.backhaus@gmail.com>
2026-01-28 10:16:35 -0500

    chore: cleanup build

--- build_dist.sh
+++ build_dist.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Configuration
-VERSION="1.0"
+VERSION="1.0.0"
 PLUGIN_ID="com.jonbackhaus.metacrawler"
 PLUGIN_UID="79832" # arbitrary but needs to be unique
-JAR_NAME="magicdraw-metacrawler-1.0-SNAPSHOT.jar"
+JAR_NAME="magicdraw-metacrawler-$VERSION.jar"
 DIST_DIR="dist"
 DIST_DATE=$(date +%Y-%m-%d)
 TEMP_DIR="dist/temp"
@@ -31,8 +31,24 @@ mkdir -p "$TEMP_DIR/plugins/$PLUGIN_ID"
 cp "target/$JAR_NAME" "$TEMP_DIR/plugins/$PLUGIN_ID/"
 cp "src/main/resources/plugin.xml" "$TEMP_DIR/plugins/$PLUGIN_ID/"
 
-# 4. Generate Resource Manager Descriptor
-# Updated with required categorization tags for MagicDraw Resource Manager
+# 4. Generate Plugin Descriptor
+cat <<EOF > "$TEMP_DIR/plugins/plugin.xml"
+<plugin
+        id="$PLUGIN_ID"
+        name="Metacrawler Plugin"
+        version="$VERSION"
+        provider-name="Jonathan Backhaus"
+        class="com.jonbackhaus.metacrawler.MetacrawlerPlugin">
+    <runtime>
+        <library name="$JAR_NAME"/>
+    </runtime>
+    <requires>
+        <api version="1.0"/>
+    </requires>
+</plugin>
+EOF
+
+# 5. Generate Resource Manager Descriptor
 cat <<EOF > "$TEMP_DIR/data/resourcemanager/$MDR_NAME"
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <resourceDescriptor critical="false" date="$DIST_DATE" description="Recursive context menu for interactive metachain crawling in MagicDraw." id="$PLUGIN_UID" name="Metacrawler Plugin" mdVersionMax="higher" mxVersionMin="17.0" restartMagicdraw="true" type="Plugin">
@@ -53,12 +69,12 @@ cat <<EOF > "$TEMP_DIR/data/resourcemanager/$MDR_NAME"
 </resourceDescriptor>
 EOF
 
-# 5. Create Zip Bundle
+# 6. Create Zip Bundle
 cd "$TEMP_DIR"
 zip -r "../../$DIST_DIR/$ZIP_NAME" . > /dev/null
 cd ../..
 
-# 6. Cleanup
+# 7. Cleanup
 rm -rf "$TEMP_DIR"
 
 echo "Distribution bundle created: $DIST_DIR/$ZIP_NAME"
